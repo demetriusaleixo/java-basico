b244e7ce0f525deafee8f08a691b7eae195e031f
Demétrius Aleixo <145219829+demetriusaleixo@users.noreply.github.com>
2024-11-20 14:24:50 -0300

    Finalização de Tipos Primitivos

--- tipos-variaveis/src/tiposVariaveis.java
+++ tipos-variaveis/src/tiposVariaveis.java
@@ -1,5 +1,15 @@
 public class tiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Tipos Primitivos
+
+        /* byte numeroMuitoPequeno = 10; // Rece números de -127 a 127
+        short numeroPequeno = 1000; //Recebe valores de -32.768 a 32.767
+        int numeroNormal = 100000; // Recebe valores de -2.147.483.648 a 2.147.483.647. É o tipo mais utilizado.
+        long numeroImenso = 10000000000L; // Recebe valores de -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807. "Deve ter um L escrito no final"
+        float numeroDecimal = 3.14F; // Recebe valores com ponto flutuante. "Deve ter um F no final"
+        double numeroDecimalMaisPreciso = 3.1415; // Recebe valores com ponto flutuante também. É o tipo mais utilizado para isso.
+        char caractereUnicode = 104; // Recebe um valor numérico que representa um caractere na tabela Unicode.
+        boolean afirmacao = false; // Recebe os valores lógicos 'true' ou 'false'
+        */
     }
 }
